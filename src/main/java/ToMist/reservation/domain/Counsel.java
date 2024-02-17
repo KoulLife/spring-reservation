a236b0c0ab21ce9345c371b91485f7ee31550c00
Koul <dongik0122@gmail.com>
2024-02-17 13:44:53 +0900

    domain 갱신
    도메인 갱신

--- src/main/java/ToMist/reservation/domain/Counsel.java
+++ src/main/java/ToMist/reservation/domain/Counsel.java
@@ -23,19 +23,27 @@ public class Counsel {
   @Column(nullable = false, updatable = false)
   private Long counselId;
 
-  @Column private LocalDateTime appliedAt;
+  @Column(columnDefinition = "datetime DEFAULT NULL COMMENT '신청일자'")
+  private LocalDateTime appliedAt;
 
-  @Column private String name;
+  @Column(columnDefinition = "varchar(12) DEFAULT NULL COMMENT '상담 요청자'")
+  private String name;
 
-  @Column private String cellPhone;
+  @Column(columnDefinition = "varchar(13) DEFAULT NULL COMMENT '전화번호'")
+  private String cellPhone;
 
-  @Column private String email;
+  @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '책임자 이메일'")
+  private String email;
 
-  @Column private String memo;
+  @Column(columnDefinition = "text DEFAULT NULL COMMENT '상담 메모'")
+  private String memo;
 
-  @Column private String address;
+  @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '주소'")
+  private String address;
 
-  @Column private String addressDetail;
+  @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '주소 상세'")
+  private String addressDetail;
 
-  @Column private String zipCode;
+  @Column(columnDefinition = "varchar(5) DEFAULT NULL COMMENT '우편번호'")
+  private String zipCode;
 }
