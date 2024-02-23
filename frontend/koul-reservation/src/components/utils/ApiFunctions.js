9bbc5dd7dfb5a86af253f80ff3b1db1e3299b860
Koul <dongik0122@gmail.com>
2024-02-23 17:44:45 +0900

    front_add_room
    프론트엔드에서 방 추가 기능을 구현

---
+++ frontend/koul-reservation/src/components/utils/ApiFunctions.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import error from "eslint-plugin-react/lib/util/error.js";
+
+export const api = axios.create({
+  baseURL: "http://localhost:8080"
+})
+
+// this function add a new room to the database
+export async function addRoom(photo, roomType, roomPrice) {
+  const formData = new FormData()
+  formData.append("photo", photo)
+  formData.append("roomType", roomType)
+  formData.append("roomPrice", roomPrice)
+
+  const response = await api.post("/rooms/add/new-room", formData)
+  if (response === 201) {
+    return true
+  } else {
+    return false
+  }
+
+  // this function gets all room types from database;
+  export async function getRoomTypes() {
+    try {
+      const response = await api.get("/rooms/add/new-room")
+      return response.data
+    } catch (error) {
+      throw new Error("Error fetching room types")
+    }
+  }
+}
