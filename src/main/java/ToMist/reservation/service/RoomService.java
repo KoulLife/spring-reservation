523ec376b79eb64687ba32c0deb59a2d1e07cab8
Koul <dongik0122@gmail.com>
2024-02-27 01:10:56 +0900

    get room
    roomId로 방을 찾는 기능 추가

--- src/main/java/ToMist/reservation/service/RoomService.java
+++ src/main/java/ToMist/reservation/service/RoomService.java
@@ -86,5 +86,8 @@ public class RoomService implements IRoomService{
     return roomRepository.save(room);
   }
 
-
+  @Override
+  public Optional<Room> getRoomById(Long roomId) {
+    return Optional.of(roomRepository.findById(roomId).get());
+  }
 }
